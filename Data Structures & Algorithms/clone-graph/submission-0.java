d67ad4cd56ae7fd3cdbbc09400d8a5da113d258c
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-05-27 23:25:24 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    private HashMap<Node, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+        Node copy = new Node(node.val);
+        map.put(node, copy);
+        for (Node neighbor : node.neighbors) {
+            copy.neighbors.add(cloneGraph(neighbor));
+        }
+        return copy;
+    }
+}
