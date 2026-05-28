ae9eff131eccaca4ee971e98823b6f43153d90b1
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-05-28 11:37:42 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int first = nums[0];
+        int second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            int current = Math.max(second, first + nums[i]);
+            first = second;
+            second = current;
+        }
+        return second;
+    }
+}
