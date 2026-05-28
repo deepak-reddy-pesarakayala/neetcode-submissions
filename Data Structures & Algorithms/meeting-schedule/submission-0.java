186fd49a1427fff10f0b9b46098c7260aa735bdf
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-05-28 15:08:14 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> a.start - b.start);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals.get(i).start < intervals.get(i - 1).end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
